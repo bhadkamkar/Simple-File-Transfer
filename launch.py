30fef8b58e0c114eea00ffd66dfde5f9c4493558
bhadkamkar <siddharth.bhadkamkar@gmail.com>
2017-03-10 12:34:27 -0800

    modify

--- launch.py
+++ launch.py
@@ -1,4 +1,10 @@
 import subprocess
+import os
+from time import sleep
 
-command1 = subprocess.Popen(["python", "server.py", "--server-ip", "127.0.0.1", "--server-port", "8080"])
-command2 = subprocess.Popen(["python", "cli.py", "--server-ip", "127.0.0.1", "--server-port", "8080"])
+
+command1 = subprocess.Popen(["python", "server.py", "--server-ip", "127.0.0.1", "--server-port", "8083"])
+command2 = subprocess.Popen(["python", "cli.py", "--server-ip", "127.0.0.1", "--server-port", "8083"])
+
+sleep(5)
+command1.terminate()
